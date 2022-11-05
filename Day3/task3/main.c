22bdeeb2f2154872106daaba924d3be1f1f7da4d
dinasoffa <101818830+dinasoffa@users.noreply.github.com>
2022-11-05 19:49:36 +0200

    Update main.c

--- Day3/task3/main.c
+++ Day3/task3/main.c
@@ -3,39 +3,34 @@
 
 int main()
 {
+   int matrix1[2][2];
+   int matrix2[2][2];
+   int matrix3[2][2];
+   printf("enter the element of matrix 1 \n");
+   for (int i=0; i<2; i++)
+   {
+       for (int j=0;j<2; j++)
+       {
+           scanf("%d", &matrix1[i][j]);
+       }
+   }
+   printf("enter the element of matrix 2 \n");
+   for (int i=0; i<2; i++)
+   {
+       for (int j=0;j<2; j++)
+       {
+           scanf("%d", &matrix2[i][j]);
+       }
+   }
+   for (int i=0; i<2; i++)
+   {
+       for (int j=0;j<2; j++)
+       {
+           matrix3[i][j]=matrix1[i][j]+matrix2[i][j];
+           printf("%d\t",matrix3[i][j]);
+       }
+   }
 
-    int mat[2][2];
-    int mat2[2][2];
-    int mat3 [2][2];
-    int i, j ,a,b,k,y;
-    printf("enter the element of matrix \n");
-
-    for(i=0;i<2;i++)
-    {
-        for(j=0;j<2;j++)
-        {
-            scanf("%d",&mat[i][j]);
-        }
-
-    }
-    printf("enter the element of matrix 2 \n");
-    for(i=0;i<2;i++)
-    {
-        for( j=0;j<2;j++)
-        {
-            scanf("%d",&mat[a][b]);
-        }
-    }
-    for (i=0; i<2; i++)
-    {
-        for (j=0; j<2; j++)
-        {
-            mat3[i][j]=mat[i][j] +mat2[i][j];
-            printf( mat3[i][j]);
-
-        }
-
-    }
 
 
     return 0;
